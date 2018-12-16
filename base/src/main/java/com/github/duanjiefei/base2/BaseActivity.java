512fab407627d869ddc6b2460287633228695be0
duanjiefei <jiefeiduan@163.com>
2018-12-16 21:16:36 +0800

    新增base Modle ,更改BuildTools的版本

---
+++ base/src/main/java/com/github/duanjiefei/base2/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.github.duanjiefei.base2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
